28d8e990baa414e538b84e24a85f5301fc31f3b9
BlackMantle <Dimasikylll@mail.ru>
2019-08-09 07:29:32 +0300

    Update

---
+++ precache-manifest.31cc8938309e49786b5941db6c7112a1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a0e93cfe137c6a15756af4dea600d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "616c41dbd0eeb026ee82",
+    "url": "/static/css/main.8d59ed35.chunk.css"
+  },
+  {
+    "revision": "b3795a6e466ad5bbaf27",
+    "url": "/static/js/2.cdb8824a.chunk.js"
+  },
+  {
+    "revision": "616c41dbd0eeb026ee82",
+    "url": "/static/js/main.62db680c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c6dc3b4c77975401ec831d075dfc52a",
+    "url": "/static/media/overviewItem4.2c6dc3b4.png"
+  },
+  {
+    "revision": "761db62c753070275ef26c5e2412de50",
+    "url": "/static/media/overviewItem6.761db62c.png"
+  },
+  {
+    "revision": "2d36a15af83de1219f06a79d58f17c65",
+    "url": "/static/media/screenshot.2d36a15a.png"
+  },
+  {
+    "revision": "643cf5ef50829cbf5023d213996b52f5",
+    "url": "/static/media/sprite1.643cf5ef.gif"
+  },
+  {
+    "revision": "3510c93a14645e7e6072ebcd23cc4237",
+    "url": "/static/media/sprite2.3510c93a.gif"
+  },
+  {
+    "revision": "3a5bccb675f32fbe5ebbf24d93f19838",
+    "url": "/static/media/sprite3.3a5bccb6.gif"
+  },
+  {
+    "revision": "2c9c7f9dfde6deebafc444918a01d0f5",
+    "url": "/static/media/sprite4.2c9c7f9d.gif"
+  },
+  {
+    "revision": "656928e2b6ff4571c071280c34d72186",
+    "url": "/static/media/sprite5.656928e2.gif"
+  },
+  {
+    "revision": "24a9894fd91635dbd193427d9b5665ae",
+    "url": "/static/media/sprite6.24a9894f.gif"
+  },
+  {
+    "revision": "39fa2a8880ada04fd5eeaa8b1a2e5865",
+    "url": "/static/media/sprite7.39fa2a88.gif"
+  },
+  {
+    "revision": "6f0779eb5d8a5043d9bc95764e6be5e0",
+    "url": "/static/media/sprite8.6f0779eb.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/piskel-clone/precache-manifest.4cd6b68d735906cef5e7312becaa7653.js"
+  "/precache-manifest.31cc8938309e49786b5941db6c7112a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/piskel-clone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
